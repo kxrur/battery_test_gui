4b48ffddeb74aae367604aea0840fa1bc2d3dbed
kxr <kxrur@gmail.com>
2025-07-01 13:28:26 -0400

    specta for IPC

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,33 @@
+use serde::{Deserialize, Serialize};
+use specta_typescript::Typescript;
+use tauri_specta::{collect_commands, Builder};
+
+//
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+#[specta::specta]
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn hello_world(name: &str) -> String {
+    dbg!(format!("Hello, {name}! You've been greeted from Rust!"))
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    let builder = Builder::<tauri::Wry>::new().commands(collect_commands![hello_world,]);
+
+    #[cfg(debug_assertions)] // <- Only export on non-release builds
+    builder
+        .export(Typescript::default(), "../src/bindings.ts")
+        .expect("Failed to export typescript bindings");
+
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(builder.invoke_handler())
+        .setup(move |app| {
+            // This is also required if you want to use events
+            builder.mount_events(app);
+
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
     <h1 class="text-3xl font-bold underline text-teal-500">Hello world!</h1>
+    <Button @click="commands.helloWorld('lel')">Greet</Button>
   </div>
 </template>
 
+<script setup lang="ts">
+import { Button } from "@/components/ui/button";
+import { commands } from "./bindings";
+</script>
